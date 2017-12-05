94dd844a66e5bf32d36fbbc2d82639fba4195920
David.Rodrigues <david@sixhat.net>
2017-12-05 22:31:39 +0000

    P5.js v 16

--- template-project/libs/p5.dave.js
+++ template-project/libs/p5.dave.js
@@ -1,3 +1,6 @@
+// David Sousa-Rodrigues
+// A set of utilities for working with P5.js
+
 //  A simple Grid System to place over drawings
 class Grid {
   constructor(){
@@ -41,33 +44,37 @@ class Grid {
   }
 }
 
+// Class with perceptual color manipulation
+// A couple of functions are from https://programmingdesignsystems.com/color/perceptually-uniform-color-spaces/index.html#perceptually-uniform-color-spaces-e7zMSWy
 class Dave {
   // Short version of functions bellow.
   constructor(){
     this.grid=new Grid();
     this.g = this.grid;
   }
+  // alias for fillHsluv
   f(h, s, l) {
     this.fillHsluv(h,s,l);
   }
+  // alias for strokeHsluv
   s(h, s, l) {
     this.strokeHsluv(h,s,l);
   }
-  // Long named versions of function where the real implementation happens.
+  // sets fill according to hsluv parameters
   fillHsluv(h, s, l) {
-    var rgb = hsluv.hsluvToRgb([h, s, l]);
+    let rgb = hsluv.hsluvToRgb([h, s, l]);
     fill(rgb[0] * 255, rgb[1] * 255, rgb[2] * 255);
   }
+  // sets stroke according to hsluv parameters
   strokeHsluv(h, s, l) {
-    var rgb = hsluv.hsluvToRgb([h, s, l]);
+    let rgb = hsluv.hsluvToRgb([h, s, l]);
     stroke(rgb[0] * 255, rgb[1] * 255, rgb[2] * 255);
   }
 }
 
 
-// A couple of functions from https://programmingdesignsystems.com/color/perceptually-uniform-color-spaces/index.html#perceptually-uniform-color-spaces-e7zMSWy
-// for perceptually uniform color spaces
 
+// Implements an L-System
 function LSystem(system) {
   this.alphabet = system.alphabet;
   this.startString = system.axiom;
@@ -100,3 +107,104 @@ LSystem.prototype.iterate = function(nIters){
   this.nIters += nIters;
   return this.endString;
 };
+
+// The Turtle class implements the basic methods of Turtle graphics from the logo languages.
+class Turtle{
+  constructor(){
+    this.x = 0;
+    this.y = 0;
+    this.penDown = false;
+    this.penColor = 0;
+    this.penWeight = 1;
+    this.heading = -HALF_PI;
+    this.stack = [];
+  }
+
+  // Pushes the current state of turtle to stack.
+  push(){
+    this.stack.push({
+      'x': this.x,
+      'y': this.y,
+      'penDown':this.penDown,
+      'penColor':this.penColor,
+      'penWeight':this.penWeight,
+      'heading':this.heading
+    });
+  }
+  // Reverts to previous saved state of the turtle removing it from the stack.
+  pop(){
+    const el = this.stack.pop();
+    this.x = el.x;
+    this.y = el.y;
+    this.penDown = el.penDown;
+    this.penColor = el.penColor;
+    this.penWeight = el.penWeight;
+    this.heading = el.heading;
+  }
+
+  // Alias to color
+  cl(c){this.color(c);}
+  color(c){
+    this.penColor = c;
+  }
+
+  // Alias to up
+  pu(){this.up();}
+  up(){
+    this.penDown=false;
+  }
+
+  // Alias to down
+  pd(){this.down();}
+  down(){
+    this.penDown= true;
+  }
+
+  // Alias to right
+  rt(r){this.right(r);}
+  right(rot){
+    this.heading+=radians(rot);
+  }
+
+  // Alias to left
+  lt(l){this.left(l);}
+  left(rot){
+    this.heading-=radians(rot);
+  }
+
+  // Set turtle position in coordinate system
+  pos(x,y){
+    this.x = x;
+    this.y = y;
+  }
+  // Alias to forward
+  fw(s){this.forward(s);}
+  forward(steps){
+    const tx = this.x+steps*cos(this.heading);
+    const ty = this.y+steps*sin(this.heading);
+
+    if (this.penDown){
+      push();
+      stroke(this.penColor);
+      strokeWeight(this.penWeight);
+      line(this.x, this.y, tx, ty);
+      pop();
+    }
+    this.x = tx;
+    this.y = ty;
+  }
+  // Alias to backward
+  bk(s){this.backward(s);}
+  backward(steps){
+    const ps = this.penDown;
+    this.penDown = false;
+    this.forward(-steps);
+    this.penDown = ps;
+  }
+  st(s){this.style(s);} // Alias
+  style(weight) {
+    this.penWeight = weight;
+  }
+  // Set heading of turtle East=0, CW
+  hd(h){this.heading=radians(h)};
+}
--- template-project/sketch.js
+++ template-project/sketch.js
@@ -1,26 +1,60 @@
-var dave;
+let turtle;
+
+
 function setup() {
-  var canvas = createCanvas(500, 500);
-  canvas.parent('p5-canvas-holder');
-  canvas.mouseClicked(restartModel);
-  restartModel();
-
-  dave =  new Dave();
-
-  dave.f(45,50,50);
-
-  rect(5,5,100,100);
-
-  dave.g.horizontal.push(45,34,100);
-  dave.g.show();
+  var canvas = createCanvas(innerWidth, innerHeight);
+  background('lemonchiffon');
+  frameRate(1);
+  turtle = new Turtle();
+  // turtle.pos(width / 2, height / 2);
+  turtle.color(color(20,255,0));
+  turtle.pd();
 }
 
-function restartModel(){
-  pixelDensity(1);
-  background(100);
-}
-
+let lsystem = 'L';// Tree
+// let lsystem = 'FX';// Dragon
 function draw() {
-  // background(0);
+  background(50);
+  print(frameCount);
+  if (frameCount===7){
+    noLoop();
+  }
+  let nova = '';
+  turtle.pos(width / 2, height/2);
+  turtle.hd(-90);
 
+
+  for (let i = 0; i < lsystem.length; i++) {
+    if (lsystem[i] === 'L') {nova+='+RF-LFL-FR+';}
+    if (lsystem[i] === 'R') {nova+='-LF+RFR+FL-';}
+    if (lsystem[i] === 'F') {nova+='F';turtle.forward(6);}
+    if (lsystem[i] === '+') {nova+='+';turtle.right(90);}
+    if (lsystem[i] === '-') {nova+='-';turtle.left(90);}
+  }
+
+
+
+  // for (let i = 0; i < lsystem.length; i++) {
+  //   if (lsystem[i] === 'X') {nova+='F[-X][X]F[-X]+FX';}
+  //   if (lsystem[i] === 'F') {nova+='FF';turtle.forward(36/frameCount);}
+  //   if (lsystem[i] === '[') {nova+='[';turtle.push();}
+  //   if (lsystem[i] === ']') {nova+=']';turtle.pop();}
+  //   if (lsystem[i] === '+') {nova+='+';turtle.color(color(20,255,50));turtle.right(25);}
+  //   if (lsystem[i] === '-') {nova+='-';turtle.color(color(20,130,0));turtle.left(25);}
+  // }
+
+
+  // Dragon
+  // for (let i = 0; i < lsystem.length; i++) {
+  //   if (lsystem[i] === 'X') nova += 'X+YF+';
+  //   if (lsystem[i] === 'Y') nova += '-FX-Y';
+  //   if (lsystem[i] === 'F') turtle.forward(12);
+  //   if (lsystem[i] === '+') {nova+='+';turtle.right(90);}
+  //   if (lsystem[i] === '-') {nova+='-';turtle.left(90);}
+  // }
+  lsystem = nova;
+}
+
+function mousePressed(){
+  noLoop();
 }
